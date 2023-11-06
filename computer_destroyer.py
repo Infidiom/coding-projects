0bab49f28978cf4b98657ed809e4a6e0808bee05
Infidiom <149966544+Infidiom@users.noreply.github.com>
2023-11-06 19:54:29 +0000

    Create computer_destroyer.py
    The first computer destroyer

---
+++ computer_destroyer.py
@@ -0,0 +1,7 @@
+i = int(input("Enter the starting number."))
+a = int(input("Enter the starting range"))
+z = int(input("Enter the final range"))
+
+for i in range(a,z):
+  print(f"The value of i is {i}.")
+  i += 1
